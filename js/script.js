c451cda66d2e6933a0b5cbea682b93070b3cb0a3
dasinua <dasinua@gmail.com>
2020-10-22 00:44:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answers = [];
+
+answers[0] = prompt("Как Вас зовут", "");
+answers[1] = prompt("Как Вас зовут2", "");
+answers[2] = prompt("Как Вас зовут3", "");
+
+document.write(answers);
